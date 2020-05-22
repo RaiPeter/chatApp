c03649f5b44a87a360898d238d13957c3106194b
RaiPeter <rai.peter2@gmail.com>
2020-05-22 22:23:00 +0530

    make it better

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const socket = io('https://chat-app-lication.herokuapp.com/:3000')
+const socket = io('https://chat-app-lication.herokuapp.com:3000')
 
 const messageContainer=document.getElementById('message-container')
 const messageForm=document.getElementById('send-container')
